9aea1d0b96f29122ccddefdd7206af558a2d6e44
Jinwoo Kim <kidjinwoo@me.com>
2025-01-01 23:10:19 +0900

    no message

--- MiscellaneousStark/MiscellaneousStark/Classes/TextView/TextViewController.mm
+++ MiscellaneousStark/MiscellaneousStark/Classes/TextView/TextViewController.mm
@@ -9,19 +9,73 @@
 #import <objc/message.h>
 #import <objc/runtime.h>
 
+/*
+ -[UISearchController _createSystemInputViewControllerIfNeededForTraitEnvironment:]
+ disableFiveRowKeyboards
+ 
+ 
+ +[UISystemInputViewController _carPlay_systemInputViewControllerForResponder:editorView:containingResponder:traitCollection:]
+ */
+
 @interface TextView : UITextView
 @end
 
 @implementation TextView
 
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (BOOL)becomeFirstResponder {
+    BOOL result = [super becomeFirstResponder];
+    
+    if (result) {
+        // TODO: 중복 확인 및 userInterfaceIdiom 변화 대응
+        if (self.traitCollection.userInterfaceIdiom == UIUserInterfaceIdiomCarPlay) {
+            __kindof UIViewController *_viewControllerForAncestor = reinterpret_cast<id (*)(id, SEL)>(objc_msgSend)(self, sel_registerName("_viewControllerForAncestor"));
+            assert(_viewControllerForAncestor != nil);
+            
+            reinterpret_cast<void (*)(Class, SEL, NSUInteger)>(objc_msgSend)(objc_lookUpClass("UISystemInputViewController"), sel_registerName("setKeyboardInteractionModel:"), 1);
+            __kindof UIViewController *systemInputViewController = reinterpret_cast<id (*)(id, SEL, id, id, id)>(objc_msgSend)(objc_lookUpClass("UISystemInputViewController"), sel_registerName("systemInputViewControllerForResponder:editorView:containingResponder:"), self, nil, _viewControllerForAncestor);
+            reinterpret_cast<void (*)(id, SEL, NSUInteger)>(objc_msgSend)(systemInputViewController, sel_registerName("setRequestedInteractionModel:"), 1);
+            reinterpret_cast<void (*)(id, SEL, id)>(objc_msgSend)(systemInputViewController, sel_registerName("setPersistentDelegate:"), self);
+            
+            //
+            
+            // -[UISearchController _createSystemInputViewControllerIfNeededForTraitEnvironment:]
+            
+            UIView *contentView = _viewControllerForAncestor.view;
+            __kindof UIView *keyboardView = systemInputViewController.view;
+            
+            [_viewControllerForAncestor addChildViewController:systemInputViewController];
+            
+            [contentView addSubview:keyboardView];
+            keyboardView.translatesAutoresizingMaskIntoConstraints = NO;
+            [NSLayoutConstraint activateConstraints:@[
+                [keyboardView.leadingAnchor constraintEqualToAnchor:contentView.safeAreaLayoutGuide.leadingAnchor],
+                [keyboardView.trailingAnchor constraintEqualToAnchor:contentView.safeAreaLayoutGuide.trailingAnchor],
+                [keyboardView.bottomAnchor constraintEqualToAnchor:contentView.safeAreaLayoutGuide.bottomAnchor],
+                [keyboardView.heightAnchor constraintEqualToConstant:[keyboardView systemLayoutSizeFittingSize:contentView.bounds.size].height]
+            ]];
+            
+            [systemInputViewController didMoveToParentViewController:_viewControllerForAncestor];
+        }
+    }
+    
+    return result;
+}
 
 @end
 
 @implementation TextViewController
 
-- (void)loadView {
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
     TextView *textView = [TextView new];
-    self.view = textView;
+    textView.frame = self.view.bounds;
+    textView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:textView];
     [textView release];
 }
 
