548fa88e13eeca06b3ce2099ab0170bc97975b6e
Jinwoo Kim <kidjinwoo@me.com>
2025-02-17 15:49:57 +0900

    no message

--- MiscellaneousUIKit/MiscellaneousUIKit/Classes/IntelligenceDemo/CloudChamberViewController.swift
+++ MiscellaneousUIKit/MiscellaneousUIKit/Classes/IntelligenceDemo/CloudChamberViewController.swift
@@ -11,51 +11,20 @@ import UIKitPrivate
 @objc
 final class CloudChamberViewController: UIViewController {
     private let cloudChamberView: UIKitPrivate._UICloudChamber = {
-        // 이걸로 만들면 수정이 안 되는건가?
         let descriptor = _UIIntelligenceLightSourceDescriptor
             .livingLight(
-                with: _UIColorPalette(colors: [.cyan, .orange, .green, .red, .purple])
+                with: _UIColorPalette(colors: [.cyan, .orange])
             )
         return descriptor._createLightSourceView(withFrame: .null) as! UIKitPrivate._UICloudChamber
     }()
     
     override func loadView() {
+        // configuration은 수정할 수 없는 구조같음. UIKit._UICloudChamber.init(frame: __C.CGRect, configuration: UIKit._UICloudChamber.Configuration) -> UIKit._UICloudChamber에서만 CAEmitterLayer가 구성되는 원리
         view = cloudChamberView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "gear"), primaryAction: nil, menu: makeMenu())
-    }
-    
-    private func makeMenu() -> UIMenu {
-        let element = UIDeferredMenuElement.uncached { [cloudChamberView] completion in
-            let configuration = cloudChamberView.configuration
-            
-            let colorsMenu: UIMenu = {
-                let addedColorActions = configuration
-                    .colors
-                    .map { color in
-                        let image = UIImage(systemName: "circle.fill")!
-                            .withTintColor(color, renderingMode: .alwaysOriginal)
-                        
-                        return UIAction(image: image) { _ in
-                            var configuration = cloudChamberView.configuration
-                            configuration.colors.removeAll { $0 == color}
-                            cloudChamberView.configuration = configuration
-                        }
-                    }
-                
-                let menu = UIMenu(options: .displayAsPalette, children: addedColorActions)
-                return menu
-            }()
-            
-            completion([
-                colorsMenu
-            ])
-        }
-        
-        return UIMenu(children: [element])
+        view.backgroundColor = .systemBackground
     }
 }
